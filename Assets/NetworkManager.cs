a7b3f00726048b0262cc43a840f4298cb7400399
Patrick Higgins <quickpanic@me.com>
2014-09-02 22:27:32 -0700

    Created NetworkManager
    Branched to implement multiplayer networking code

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		 Connect ();
+	}
+	void Connect () {
+		PhotonNetwork.ConnectUsingSettings("alpha-1");
+		Debug.Log ("Connected Successfully");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void onGUI () {
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString() );
+	}
+}
