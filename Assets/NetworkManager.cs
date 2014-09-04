0d8b71d726392f0b389c33bb8550c3bd289321ad
Patrick Higgins <quickpanic@me.com>
2014-09-03 23:16:04 -0700

    Drafted network code

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -1,21 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkManager : MonoBehaviour {
-
+public class NetworkManager : MonoBehaviour {   
+	SpawnSpot[] spawnSpots;
+	
+	public GameObject standbyCamera;
+	
+	public bool offlineMode = false;
+	
 	// Use this for initialization
 	void Start () {
-		 Connect ();
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
+		Connect ();
 	}
-	void Connect () {
-		PhotonNetwork.ConnectUsingSettings("alpha-1");
-		Debug.Log ("Connected Successfully");
-	}
-	// Update is called once per frame
-	void Update () {
 	
+	// Update is called once per frame
+	void Connect () {
+		if(offlineMode){
+			PhotonNetwork.offlineMode = true;
+		}
+		else{
+			PhotonNetwork.ConnectUsingSettings ("alpha-0");
+		}
 	}
-	void onGUI () {
-		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString() );
+	
+	void OnGUI (){
+		GUILayout.Label ( PhotonNetwork.connectionStateDetailed.ToString() );
 	}
-}
+	
+	void OnJoinedLobby (){
+		Debug.Log ("Joined Lobby");
+		PhotonNetwork.JoinRandomRoom ();
+	}
+	
+	void OnPhotonRandomJoinFailed (){
+		Debug.Log ("RandomJoin Failed");
+		PhotonNetwork.CreateRoom ( null );
+	}
+	
+	void OnJoinedRoom (){
+		Debug.Log ("OnJoinRoom");
+		
+		SpawnPlayer();
+	}
+	
+	void SpawnPlayer(){
+		
+		if(spawnSpots == null){
+			Debug.LogError ("WTF?!? U FORGOT TO MAKE SPAWNPOINTS");
+			return;
+		}
+		PhotonNetwork.Instantiate ("PlayerController", Vector3.zero, Quaternion.identity, 1);
+	    SpawnSpot mySpawnSpot = spawnSpots[ Random.Range (0, spawnSpots.Length) ];
+		GameObject myPlayerGO = PhotonNetwork.Instantiate ("PlayerController", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 1);
+
+		standbyCamera.SetActive(false);
+		
+/*		((MonoBehaviour)myPlayerGO.GetComponent("FPSInput Controller")).enabled = true;
+		((MonoBehaviour)myPlayerGO.GetComponent("Mouse Look")).enabled = true;
+		myPlayerGO.transform.FindChild ("Main Camera").gameObject.SetActive(true);
+*/
+	}
+} ﻿
---
+++ Assets/SpawnSpot.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSpot : MonoBehaviour {
+
+	public int TeamID = 1;
+}
