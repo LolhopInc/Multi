d1b3f25bc84ae8a2909b226fce8aeb41a65f648b
Patrick Higgins <quickpanic@me.com>
2014-09-04 20:55:30 -0700

    Fixed Network Functionality

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -3,19 +3,47 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
 
+	public Camera standbyCamera;
+	SpawnSpot [] spawnSpots;
 	// Use this for initialization
 	void Start () {
+		 spawnSpots = GameObject.FindObjectsOfType<SpawnSpot> ();
 		 Connect ();
 	}
+
 	void Connect () {
 		PhotonNetwork.ConnectUsingSettings("alpha-1");
 		Debug.Log ("Connected Successfully");
 	}
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
-	void onGUI () {
+
+	void OnGUI () {
 		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString() );
 	}
+
+	void OnJoinedLobby() {
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed() {
+		PhotonNetwork.CreateRoom (null);
+
+	}
+
+	void OnJoinedRoom() {
+
+		if (spawnSpots == null) {
+			Debug.Log("No spawn points found");
+			return;
+		}
+
+		SpawnSpot mySpawnSpot = spawnSpots [Random.Range (0, spawnSpots.Length)];
+
+		PhotonNetwork.Instantiate ("PlayerController", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
+		standbyCamera.enabled = false;
+		}
 }
