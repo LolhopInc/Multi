ac769c2e34819a368050af3f1e567d814665df13
Patrick Higgins <quickpanic@me.com>
2014-09-04 19:24:23 -0700

    Revert Massive Error Cascade
    Rollback to commit a7b3f00726048b0262cc43a840f4298cb7400399.

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -1,64 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkManager : MonoBehaviour {   
-	SpawnSpot[] spawnSpots;
-	
-	public GameObject standbyCamera;
-	
-	public bool offlineMode = false;
-	
+public class NetworkManager : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
-		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
-		Connect ();
+		 Connect ();
 	}
-	
-	// Update is called once per frame
 	void Connect () {
-		if(offlineMode){
-			PhotonNetwork.offlineMode = true;
-		}
-		else{
-			PhotonNetwork.ConnectUsingSettings ("alpha-0");
-		}
+		PhotonNetwork.ConnectUsingSettings("alpha-1");
+		Debug.Log ("Connected Successfully");
 	}
+	// Update is called once per frame
+	void Update () {
 	
-	void OnGUI (){
-		GUILayout.Label ( PhotonNetwork.connectionStateDetailed.ToString() );
 	}
-	
-	void OnJoinedLobby (){
-		Debug.Log ("Joined Lobby");
-		PhotonNetwork.JoinRandomRoom ();
+	void onGUI () {
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString() );
 	}
-	
-	void OnPhotonRandomJoinFailed (){
-		Debug.Log ("RandomJoin Failed");
-		PhotonNetwork.CreateRoom ( null );
-	}
-	
-	void OnJoinedRoom (){
-		Debug.Log ("OnJoinRoom");
-		
-		SpawnPlayer();
-	}
-	
-	void SpawnPlayer(){
-		
-		if(spawnSpots == null){
-			Debug.LogError ("WTF?!? U FORGOT TO MAKE SPAWNPOINTS");
-			return;
-		}
-		PhotonNetwork.Instantiate ("PlayerController", Vector3.zero, Quaternion.identity, 1);
-	    SpawnSpot mySpawnSpot = spawnSpots[ Random.Range (0, spawnSpots.Length) ];
-		GameObject myPlayerGO = PhotonNetwork.Instantiate ("PlayerController", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 1);
-
-		standbyCamera.SetActive(false);
-		
-/*		((MonoBehaviour)myPlayerGO.GetComponent("FPSInput Controller")).enabled = true;
-		((MonoBehaviour)myPlayerGO.GetComponent("Mouse Look")).enabled = true;
-		myPlayerGO.transform.FindChild ("Main Camera").gameObject.SetActive(true);
-*/
-	}
-} ﻿
+}
