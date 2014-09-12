3ea5c3de37e39849f911dca04ab03d39e41d7ebe
Patrick Higgins <quickpanic@me.com>
2014-09-12 11:06:11 -0700

    Create AdvMath.cpp

---
+++ Assets/AdvMath.cpp
@@ -0,0 +1,13 @@
+#include<cstdlib>
+#include<cmath>
+
+using namespace std;
+
+struct roots {
+float r1;
+float r2;
+}
+roots quadratic(float a, float b, float c) {
+struct roots r;
+r.r1 = -b + sqrt(b * b - 4 * a * c) / 2 * a
+}
